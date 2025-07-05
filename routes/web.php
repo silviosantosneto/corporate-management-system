8460a53d68d7a986a8f2c0b1747a1f0d976b9b49
Silvio Santos Neto <silviosantosneto@gmail.com>
2025-07-05 16:12:57 -0300

    feat: create EconomicGroup resource controller and routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EconomicGroupController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,5 +23,9 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::group( ['middleware' => ['auth']], function () {;
+    Route::resource('economic-groups', EconomicGroupController::class);
+});
 
 require __DIR__.'/auth.php';
